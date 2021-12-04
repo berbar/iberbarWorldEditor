7c92e89d14c4c2715e3d563425e32d405369419f
lujuhong <bigbigbir@hotmail.com>
2021-12-04 18:37:33 +0800

    test

---
+++ Projects/Src/Common/data_type_definitions.ts
@@ -0,0 +1,47 @@
+
+
+export enum UBaseDataType
+{
+    Undefined,
+    Int,
+    Int64,
+    Float,
+    String,
+    Bool,
+    Id,
+}
+
+
+export type UStructField =
+{
+    Type: UBaseDataType;
+    Comment: string;
+    Database?: string;
+};
+
+export type UStruct =
+{
+    Extends: string;
+    Comment: string;
+    Fields: { [ key: string]: UStructField };
+};
+
+export type UEnumField =
+{
+    Comment: string;
+    Value: number | string;
+};
+
+export type UEnum =
+{
+    Comment: string;
+    Fields: { [ key: string]: UEnumField };
+};
+
+
+export type UDatabase =
+{
+    Name: string;
+    Type: string;
+    Comment: string;
+};

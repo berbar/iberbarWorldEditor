abbcf12af0d83ab12a5caac67559724988131fe2
lujuhong <bigbigbir@hotmail.com>
2021-12-08 18:29:50 +0800

    test

--- Projects/Src/Server/ipc_channel/manager.ts
+++ Projects/Src/Server/ipc_channel/manager.ts
@@ -1,11 +1,24 @@
 
 
+import { ipcMain, IpcMainEvent } from "electron";
 import iberbar from "../libs/iberbar";
 import { CIpcChannelEventAttribute, CIpcChannelAttribute } from "./attributes";
 
+
+type UChannelNode =
+{
+    name: string;
+    type: iberbar.System.Reflection.CType;
+    instance: Object;
+    persistent: boolean;
+    events: { [ key: string ]: iberbar.System.Reflection.CMethodInfo };
+}
+
+
 export class CIpcChannelManager
 {
-    protected m_channelDefs: void = null;
+    protected m_ioc: iberbar.Autofac.IContainer = null;
+    protected m_channels: { [ key: string ]: UChannelNode } = null;
 
     public Register( clsType: iberbar.System.Reflection.CType ): void
     {
@@ -15,11 +28,75 @@ export class CIpcChannelManager
             console.log("");
             return;
         }
-        attrChannel.Name
+        let channelNode = this.m_channels[attrChannel.Name];
+        if ( channelNode != null )
+        {
+            console.log( "" );
+            return;
+        }
+        let events = {};
+        let methods = channelNode.type.GetMethods();
+        for ( let i = 0, s = methods.length; i < s; i ++ )
+        {
+            let methodTemp = methods[ i ];
+            let attrEvent = methodTemp.GetCustomAttributeOne( iberbar.System.Reflection.TypeOf( CIpcChannelEventAttribute ) );
+            if ( attrEvent == null )
+                continue;
+            events[ attrEvent.eventId ] = methodTemp;
+        }
+        channelNode = {
+            name: attrChannel.Name,
+            type: clsType,
+            instance: null,
+            persistent: attrChannel.Persistent,
+            events: events
+        };
+        this.m_channels[ attrChannel.Name ] = channelNode;
+
+
     }
 
-    public Load(): void
+    public LoadAllPersistent(): void
     {
+        for ( const name in this.m_channels )
+        {
+            let channelNode = this.m_channels[ name ];
+            if ( channelNode.instance != null )
+                continue;
+            if ( channelNode.persistent == false )
+                continue;
+            this.LoadForce( channelNode );
+        }
+    }
+
+    public LoadOne( name: string ): void
+    {
+        let channelNode = this.m_channels[ name ];
+        if ( channelNode.instance != null )
+            return;
+        this.LoadForce( channelNode );
+    }
+
+    protected LoadForce( channelNode: UChannelNode ): void
+    {
+        //channelNode.instance = channelNode.type.GetConstructor().Invoke();
+        if ( channelNode.instance == null )
+        {
+            channelNode.instance = this.m_ioc.Resolve( channelNode.type );
+        }
         
+        if ( channelNode.persistent == true )
+        {
+            ipcMain.on( channelNode.name, this.OnCommonChannel.bind( this ) );
+        }
+        else
+        {
+            ipcMain.once( channelNode.name, this.OnCommonChannel.bind( this ) );
+        }
+    }
+
+    protected OnCommonChannel( event: IpcMainEvent, ...args: any[] )
+    {
+
     }
 };
